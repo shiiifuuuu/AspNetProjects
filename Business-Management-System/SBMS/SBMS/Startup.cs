ab407bbaa2e7d770a20f2183f0ece2cba5020c4e
shiiifuuuu <mdshahadat.hossain7@gmail.com>
2021-02-23 18:40:09 +0600

    initialProjectTemplate

---
+++ Business-Management-System/SBMS/SBMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SBMS.Startup))]
+namespace SBMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
